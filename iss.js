b290285dbc4fcf78080831627f8926430a6a14cd
Rajan Lail <rajanlail3993@gmail.com>
2023-06-17 14:43:38 -0700

    Add nextISSTimesForMyLocation. Refractor code.

--- iss.js
+++ iss.js
@@ -1,5 +1,41 @@
 const request = require('request');
 
+// ... other three functions not included in solution ...
+
+/**
+ * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
+ * Input:
+ *   - A callback with an error or results.
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly-over times as an array (null if error):
+ *     [ { risetime: <number>, duration: <number> }, ... ]
+ */
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, coordinates) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(coordinates, (error, flyOverTimes) => {
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, flyOverTimes);
+      });
+    });
+  });
+};
+
+module.exports = { nextISSTimesForMyLocation };
+
+
 const fetchMyIP = function(callback) { //API Call #1
   request('https://api.ipify.org?format=json', (error, response, body) => {
     if (error) {
@@ -67,4 +103,3 @@ const fetchISSFlyOverTimes = function(coords, callback) { //API Call #3
 };
 
 
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
